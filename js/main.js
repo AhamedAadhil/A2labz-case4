93be5c904a5c4a8d1eb4f6f254dee5cc42d35bfe
Aadhil Shihabdeen <it21004490@my.sliit.lk>
2024-02-12 14:46:02 +0530

    floating button done

--- js/main.js
+++ js/main.js
@@ -67,6 +67,19 @@
         return false;
     });
 
+    // flt-icn
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.flt-icn').fadeIn('slow');
+        } else {
+            $('.flt-icn').fadeOut('slow');
+        }
+    });
+    // $('.flt-icn').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
+
 
     // Header carousel
     $(".header-carousel").owlCarousel({
