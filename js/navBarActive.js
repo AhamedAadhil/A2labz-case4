daf21b4464e50bd24617e1d5ec4c65fb34501e07
Ahamed Aathil <ahamedaathil.5@gmail.com>
2026-01-06 19:58:22 +0530

    Ignore .DS_Store files

--- js/navBarActive.js
+++ js/navBarActive.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get all navbar links
   var navLinks = document.querySelectorAll(".navbar-nav .nav-link");
 
-  // Add click event listener to each link
+  // Set active on initial load based on current path
+  try {
+    var currentPage = window.location.pathname.split("/").pop() || "index.html";
+    navLinks.forEach(function (link) {
+      var href = link.getAttribute("href");
+      if (href === currentPage) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  } catch (e) {}
+
+  // Add click handler to update active class on navigation
   navLinks.forEach(function (link) {
     link.addEventListener("click", function () {
-      // Remove 'active' class from all links
       navLinks.forEach(function (navLink) {
         navLink.classList.remove("active");
       });
-
-      // Add 'active' class to the clicked link
       link.classList.add("active");
     });
   });
